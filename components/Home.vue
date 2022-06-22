6380d0bcf909ac8ce4473713005970e12e820bbd
khmuhib <khmuhib2013@gmail.com>
2022-06-22 10:15:55 +0600

    home page modify

--- components/Home.vue
+++ components/Home.vue
@@ -4,20 +4,279 @@
     <CallToAction></CallToAction>
     <Hero />
 
+    <div class="px-4 py-3 text-center bg-light">
+      <h1 class="display-5 fw-bold">Free WordPress Themes</h1>
+      <div class="col-md-6 mx-auto">
+        <p class="lead mb-4">
+          We are excited to be part of the WordPress community and looking to
+          make contribution by releasing free WordPress themes for everyone to
+          use. Other themes can be <a href="#">found here.</a>
+        </p>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="home-card-main">
+              <img src="images/p1.jpg" alt="" />
+              <h3>Shapely</h3>
+              <p>
+                Shapely is a nature inspired One Page WordPress theme ready to
+                take any website to the next level.
+              </p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="home-card-main">
+              <img src="images/p1.jpg" alt="" />
+              <h3>Shapely</h3>
+              <p>
+                Shapely is a nature inspired One Page WordPress theme ready to
+                take any website to the next level.
+              </p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="home-card-main">
+              <img src="images/p1.jpg" alt="" />
+              <h3>Shapely</h3>
+              <p>
+                Shapely is a nature inspired One Page WordPress theme ready to
+                take any website to the next level.
+              </p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="home-card-main">
+              <img src="images/p1.jpg" alt="" />
+              <h3>Shapely</h3>
+              <p>
+                Shapely is a nature inspired One Page WordPress theme ready to
+                take any website to the next level.
+              </p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="home-card-main">
+              <img src="images/p1.jpg" alt="" />
+              <h3>Shapely</h3>
+              <p>
+                Shapely is a nature inspired One Page WordPress theme ready to
+                take any website to the next level.
+              </p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="home-card-main">
+              <img src="images/p1.jpg" alt="" />
+              <h3>Shapely</h3>
+              <p>
+                Shapely is a nature inspired One Page WordPress theme ready to
+                take any website to the next level.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
+    <div class="px-4 py-3 text-center bg-light">
+      <h1 class="display-5 fw-bold">Primium WordPress Themes</h1>
+      <div class="col-md-6 mx-auto">
+        <p class="lead mb-4">
+          If yo are looking for more featured and customization options then
+          premium WordPress themes are the right fit for you. Best examples can
+          be <a href="#">found here.</a>
+        </p>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="home-card-main">
+              <img src="images/p1.jpg" alt="" />
+              <h3>Shapely</h3>
+              <p>
+                Shapely is a nature inspired One Page WordPress theme ready to
+                take any website to the next level.
+              </p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="home-card-main">
+              <img src="images/p1.jpg" alt="" />
+              <h3>Shapely</h3>
+              <p>
+                Shapely is a nature inspired One Page WordPress theme ready to
+                take any website to the next level.
+              </p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="home-card-main">
+              <img src="images/p1.jpg" alt="" />
+              <h3>Shapely</h3>
+              <p>
+                Shapely is a nature inspired One Page WordPress theme ready to
+                take any website to the next level.
+              </p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="home-card-main">
+              <img src="images/p1.jpg" alt="" />
+              <h3>Shapely</h3>
+              <p>
+                Shapely is a nature inspired One Page WordPress theme ready to
+                take any website to the next level.
+              </p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="home-card-main">
+              <img src="images/p1.jpg" alt="" />
+              <h3>Shapely</h3>
+              <p>
+                Shapely is a nature inspired One Page WordPress theme ready to
+                take any website to the next level.
+              </p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="home-card-main">
+              <img src="images/p1.jpg" alt="" />
+              <h3>Shapely</h3>
+              <p>
+                Shapely is a nature inspired One Page WordPress theme ready to
+                take any website to the next level.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
+    <div class="px-4 py-3 text-center bg-light">
+      <h1 class="display-5 fw-bold">From Our Blog</h1>
+      <div class="col-md-6 mx-auto">
+        <p class="lead mb-4">
+          Here are the latest posts on web design, graphic design, development
+          and WordPress <a href="#">Go To Our Blog</a>
+        </p>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="home-blog-main">
+              <h4>
+                <a href="#"
+                  >Shapely is a nature inspired One Page WordPress theme</a
+                >
+              </h4>
+              <h6>
+                <span class="text-muted">Posted on Tuesday June 14, 2022</span>
+              </h6>
+              <p>
+                Shapely is a nature inspired One Page WordPress theme ready to
+                take any website to the next level...
+              </p>
+            </div>
+            <div class="home-blog-main">
+              <h4>
+                <a href="#"
+                  >Shapely is a nature inspired One Page WordPress theme</a
+                >
+              </h4>
+              <h6>
+                <span class="text-muted">Posted on Tuesday June 14, 2022</span>
+              </h6>
+              <p>
+                Shapely is a nature inspired One Page WordPress theme ready to
+                take any website to the next level...
+              </p>
+            </div>
+            <div class="home-blog-main">
+              <h4>
+                <a href="#"
+                  >Shapely is a nature inspired One Page WordPress theme</a
+                >
+              </h4>
+              <h6>
+                <span class="text-muted">Posted on Tuesday June 14, 2022</span>
+              </h6>
+              <p>
+                Shapely is a nature inspired One Page WordPress theme ready to
+                take any website to the next level...
+              </p>
+            </div>
+            <div class="home-blog-main">
+              <h4>
+                <a href="#"
+                  >Shapely is a nature inspired One Page WordPress theme</a
+                >
+              </h4>
+              <h6>
+                <span class="text-muted">Posted on Tuesday June 14, 2022</span>
+              </h6>
+              <p>
+                Shapely is a nature inspired One Page WordPress theme ready to
+                take any website to the next level...
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
+    <div class="px-4 py-3 text-center bg-light">
+      <h1 class="display-5 fw-bold">Subscribe For More!</h1>
+      <div class="col-md-6 mx-auto">
+        <p class="lead mb-4">
+          If you want to be the first to find out more about our colorful
+          WordPress themes, please subscribe to our newsletter
+        </p>
+      </div>
+      <div class="col-md-6 mx-auto">
+        <div class="input-group mb-3">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Enter your Email"
+            aria-label="Enter your Email"
+            aria-describedby="basic-addon2"
+          />
+          <button class="input-group-text btn btn-primary" id="basic-addon2">Subscribe</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Hero from './Hero.vue';
-import CallToAction from './CallToAction.vue';
+import Hero from "./Hero.vue";
+import CallToAction from "./CallToAction.vue";
 export default {
   name: "home",
-  components: { Hero, CallToAction }
+  components: { Hero, CallToAction },
 };
 </script>
 
-<style>
+<style scoped>
+.home-card-main img {
+  padding: 5px;
+  width: 100%;
+}
+
+.home-card-main:hover {
+  box-shadow: 0px 0px 10px 0px #ccc;
+}
+
+.home-blog-main {
+  display: table;
+  padding: 10px 20px;
+  margin: auto;
+  margin: 10px auto;
+}
+
+.home-blog-main:hover {
+  box-shadow: 0px 0px 10px 0px #ccc;
+}
 </style>
--- components/inc/AppheaderTop.vue
+++ components/inc/AppheaderTop.vue
@@ -1,34 +1,32 @@
 <template>
-
-    <div class="">
-
-        <div class="py-3 bg-warning">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-3">
-                        <NuxtLink to="/"><img src="/images/logo.png" alt="" class="header-top-logo-img"></NuxtLink>
-                    </div>
-                    <div class="col-md-6 text-right">
-                        <a href="#">
-                            <img src="/images/header_top_ads.jpg" alt="" class="header-top-ads-img" />
-                        </a>
-                    </div>
-                    <div class="col-md-3 text-right">
-                        <a href="" class="btn btn-success">Login</a>
-                        <a href="" class="btn btn-primary">Sign up</a>
-                    </div>
-                </div>
-
-            </div>
+  <div class="py-3 bg-warning">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <NuxtLink to="/"
+            ><img src="/images/logo.png" alt="" class="header-top-logo-img"
+          /></NuxtLink>
         </div>
+        <div class="col-md-6 text-right">
+          <a href="#">
+            <img
+              src="/images/header_top_ads.jpg"
+              alt=""
+              class="header-top-ads-img"
+            />
+          </a>
+        </div>
+        <div class="col-md-3 text-right">
+          <a href="" class="btn btn-success">Login</a>
+          <a href="" class="btn btn-primary">Sign up</a>
+        </div>
+      </div>
     </div>
-
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style scoped>
